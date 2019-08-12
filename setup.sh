221de4c636b6d3cd9e25e25a5a22fd0bcb51dd7d
seryishay <seryishay@gmail.com>
2019-08-12 11:30:32 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 # Install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Copy .vimrc
-cp ./.vimrc ~/.vimrc
-
 # Install configuration
 vi -c ':PluginInstall' -c 'qa!'
+
+# Copy .vimrc
+cp ./.vimrc ~/.vimrc
 
 # Copy tmux configuration
 cp ./.tmux.conf ~/.tmux.conf
