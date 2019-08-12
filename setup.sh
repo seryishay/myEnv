2c448edcefd80966adfab131efa4c36e75b39c99
seryishay <seryishay@gmail.com>
2019-08-12 11:43:11 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,12 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Install configuration
 vi -c ':PluginInstall' -c 'qa!'
 
-# Copy .vimrc
+# Copy vim configuration
 cp ./.vimrc ~/.vimrc
 
 # Copy tmux configuration
 cp ./.tmux.conf ~/.tmux.conf
+
+# Make bash prompt nicer
+echo "export PS1='\[\e[35m\u\] \[\e[36m\w\] \[\e[33m\]\[\e[1m\]$ \[\e[0m\]'" >> ~/.bashrc
+source ~/.bashrc
